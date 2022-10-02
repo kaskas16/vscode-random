74b22be3c0b61522b6630265cea56e3c1c4e994b
kaskas16 <kleticiasamayoa@yahoo.ca>
2022-10-02 14:38:43 -0400

    feature: add `roll 6 dice` command
    The commit adds a command to the extension to roll a 6 dice randomly.
    
    Signed-off-by: kaskas16 <kleticiasamayoa@yahoo.ca>

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,16 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    console.log('Congratulations, your extension "vscode-random" is now active!');
-    let disposable = vscode.commands.registerCommand('vscode-random.helloWorld', () => {
-        vscode.window.showInformationMessage('Hello World from vscode-random!');
+    let disposable = vscode.commands.registerCommand('vscode-random.rollDice', () => {
+        const result = rollDice();
+        vscode.window.showInformationMessage(`Dice rolled a ${result}.`);
     });
 
     context.subscriptions.push(disposable);
 }
 
 export function deactivate() { }
+
+function rollDice(): number {
+    return 1 + Math.floor(Math.random() * 6);
+}
